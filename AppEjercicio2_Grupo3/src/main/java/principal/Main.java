3c93156eb41372791a27efd8425be529f31a8b77
gonzalopez-ml <gonzalo.glopez@mercadolibre.com>
2023-05-31 20:10:36 -0300

    use beans in main tp2

--- AppEjercicio2_Grupo3/src/main/java/principal/Main.java
+++ AppEjercicio2_Grupo3/src/main/java/principal/Main.java
@@ -2,21 +2,26 @@ package principal;
 
 import entidad.Usuario;
 import negocio.UsuarioNegocio;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import resources.Config;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Usuario usuario = new Usuario();
-	    usuario.setUsuario("Pepe");
-	    usuario.setContrasenia("123");
-	    
-	    UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
-	    boolean estado= usuarioNegocio.agregarUsuario(usuario);
-	    if(estado)
-	    	System.out.println("Se agrego correctamente");
-	    else
-	    	System.out.println("No se pudo agregar, el usuario ya existe en la BD");
-		    	
-	}	
+
+		ApplicationContext appContext = new AnnotationConfigApplicationContext(Config.class);
+
+		Usuario usuario = (Usuario)appContext.getBean("usuario");
+		System.out.println(usuario);
+
+		UsuarioNegocio usuarioNegocio = (UsuarioNegocio)appContext.getBean("usuarioNegocio");
+		boolean estado = usuarioNegocio.agregarUsuario(usuario);
+
+		if(estado)
+			System.out.println("Se agrego correctamente");
+		else
+			System.out.println("No se pudo agregar, el usuario ya existe en la BD");
+
+	}
 }
